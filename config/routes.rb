5d344cc8b6e3c13b44216ec7427384827e06dc9f
Ivan Turkovic <me@ivanturkovic.com>
2011-10-29 10:53:32 +0200

    small layout fix for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -27,6 +27,22 @@ Hipscan::Application.routes.draw do
     
   end
 
+  # devise_for :saas_admins
+  namespace "saas_admin" do
+    root :to => "subscriptions#index"
+    resources :subscriptions do
+      member do
+        post :charge
+      end
+    end
+
+    resources :accounts
+    resources :subscription_plans, :path => 'plans'
+    resources :subscription_discounts, :path => 'discounts'
+    resources :subscription_affiliates, :path => 'affiliates'
+  end
+
+
   root :to => "accounts#dashboard"
 
   #
