427c66e634052c6e245dfb8fade1cdccb41413ac
Ivan Turkovic <me@ivanturkovic.com>
2011-11-04 00:59:19 +0100

    fix

--- vendor/plugins/saas/app/models/subscription_notifier.rb
+++ vendor/plugins/saas/app/models/subscription_notifier.rb
@@ -1,10 +1,13 @@
 class SubscriptionNotifier < ActionMailer::Base
   include ActionView::Helpers::NumberHelper
-  include ApplicationHelper
 
   layout 'notifier' # use notifier.(html|text).erb as the layout
   default :from => "noreply@hipscan.com"
   default_url_options[:host] = APP_CONFIG[:host]
+
+  helper :application
+
+  content_type "text/html"
   
   def setup_email(to, subject, from = Saas::Config.from_email)
     @sent_on = Time.now
