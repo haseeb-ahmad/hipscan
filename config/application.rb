9c8b48631a284fda54cc90e69145dab2adb6a125
haseeb <engg.haseeb@gmail.com>
2013-03-05 05:25:05 -0500

    logging

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Hipscan
     # Added by the Rails HTML5 Boilerplate Template
     # https://github.com/russfrisch/Rails-HTML5-Boilerplate-Template, written by Russ Frisch
     config.action_view.javascript_expansions[:defaults] = %w(jquery rails plugins)
-  
+    self.allow_forgery_protection = false 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
